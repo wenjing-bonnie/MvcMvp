c05355f3c9c96817662ff47cd3cbf3741bd40864
wenjing.liu <jingmiancao@j1.com>
2021-01-28 16:33:10 +0800

    add parent activity

--- app/src/main/java/com/android/mvcmcp/mvc/MvcActivity.java
+++ app/src/main/java/com/android/mvcmcp/mvc/MvcActivity.java
@@ -11,6 +11,5 @@ public class MvcActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvc);
-
     }
 }
